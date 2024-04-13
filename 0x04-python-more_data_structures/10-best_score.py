23e28bf2b7d45f171829322ea94129aa9f66bb37
Udoozy <agbo4samson@gmail.com>
2024-04-13 13:35:05 -0700

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,7 +5,7 @@ def best_score(a_dictionary):
         size = 0
         largestKey = ""
         for i in my_keys:
-            if a_dictionary.keys > size:
+            if a_dictionary[i] > size:
                 size = a_dictionary[i]
                 largestKey = i
         return largestKey
