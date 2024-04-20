e74bbcff3c8378466ba45b13aecec3a67f82e3df
Udoozy <agbo4samson@gmail.com>
2024-04-20 14:36:28 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This is for square module"""
+
+
+class Square:
+    """The class definition"""
+    def __init__(self, size):
+        """Constructor for __init__.
+        Args:
+            size: lenght of the square
+        """
+        self.__size = size
