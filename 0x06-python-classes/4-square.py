244c0a0abe3f2642432bf5933a9e4ebe5009816c
Udoozy <agbo4samson@gmail.com>
2024-04-21 03:41:03 -0700

    4-square.py 5-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Module to compute size of square"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """initializing a new square size
+        Args:
+            size(int): Size of the Square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set size and value of the square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """This returns the actual area vlaue"""
+        return (self.__size * self.__size)
