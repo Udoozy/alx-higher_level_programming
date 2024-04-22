d7f922880a168b2d6f70967e745a56090eb2f46d
Udoozy <agbo4samson@gmail.com>
2024-04-22 12:40:23 -0700

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,11 +10,13 @@ def add_integer(a, b=98):
     """User defined function that add two integers
     a,b(int): The first Argument
     """
-    if not isinstance(a, (int, float)):
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    elif type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-
-    a = int(a)
-    b = int(b)
-    return (a + b)
+    else:
+        if type(a) is float:
+            a = int(a)
+        if type(b) is float:
+            b = int(b)
+        return a + b
