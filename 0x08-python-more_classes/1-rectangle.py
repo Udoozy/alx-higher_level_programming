5b4562e82767213d0b4058874d132ab80295ea84
Udoozy <agbo4samson@gmail.com>
2024-04-25 07:28:52 -0700

    1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Full definition of recangle class"""
+
+
+class Rectangle:
+    """Defined class"""
+
+    def __init__(self, height=0, width=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width_setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height_setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
