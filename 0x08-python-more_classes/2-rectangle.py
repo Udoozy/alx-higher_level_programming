74457b0e8304e2495a37e028cec1a9526c727bde
Udoozy <agbo4samson@gmail.com>
2024-04-25 08:38:49 -0700

    2-rectangle.py

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Full definition of recangle class"""
+
+
+class Rectangle:
+    """Defined class"""
+
+    def __init__(self, height=0, width=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width_setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height_setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """pub instance method-AREA"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """pub instance method - Perimeter"""
+        if self.width = 0 or self.height = 0:
+            return 0
+        return 2 * self.width + 2 * self.height
