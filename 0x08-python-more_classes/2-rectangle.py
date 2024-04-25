6c384c3b9f37e27279c5acdd557a4ce96d5f8c3d
Udoozy <agbo4samson@gmail.com>
2024-04-25 08:46:22 -0700

    2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -13,7 +13,7 @@ class Rectangle:
     def width(self):
         return self.__width
 
-    @width_setter
+    @width.setter
     def width(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
@@ -25,7 +25,7 @@ class Rectangle:
     def height(self):
         return self.__height
 
-    @height_setter
+    @height.setter
     def height(self, value):
         if type(value) is not int:
             raise TypeError("height must be an integer")
