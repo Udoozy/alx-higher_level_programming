e5068728e9c6bc92ba5b2b304d6633aae82f5b61
Udoozy <agbo4samson@gmail.com>
2024-04-25 10:11:57 -0700

    4-rectangle.py

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Full definition of recangle class"""
+
+
+class Rectangle:
+    """Defined class"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width is 0 or self.height is 0:
+            return 0
+        return 2 * self.width + 2 * self.height
+
+    def __str__(self):
+        """print the Rect"""
+        if self.width is 0 or self.height is 0:
+            return ""
+
+    return "{}{}".format(
+        ('#' * self.width + '\n') * (self.height - 1), '#' * self.width)
+
+    def __repr__(self):
+        """Prints representation of rectangle"""
+        return 'Rectangle({}, {})'.format(self.width, self.height)
