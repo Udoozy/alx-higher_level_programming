9f77d1891df1aae675509e5d7f3340f1418c2bc0
Udoozy <agbo4samson@gmail.com>
2024-04-25 08:51:04 -0700

    2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -5,9 +5,9 @@
 class Rectangle:
     """Defined class"""
 
-    def __init__(self, height=0, width=0):
-        self.height = height
+    def __init__(self, width=0, height=0):
         self.width = width
+        self.height = height
 
     @property
     def width(self):
