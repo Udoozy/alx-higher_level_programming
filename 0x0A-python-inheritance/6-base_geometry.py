4b6df60acafc311dc964512d7381cd7c2f5ede6c
Udoozy <agbo4samson@gmail.com>
2024-04-30 14:11:49 -0700

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module for task six"""
+
+
+class BaseGeometry:
+    """Class that define an area"""
+
+    def area(self):
+        """Unimplemented area"""
+        raise Exception("area() is not implemented")
