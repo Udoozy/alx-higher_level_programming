ccaaf7b0a2e6575f14693a6a49a6c7484a200d50
Udoozy <agbo4samson@gmail.com>
2024-04-30 13:22:01 -0700

    2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Check for instance"""
+'''Check for instance'''
 
 
 def is_same_class(obj, a_class):
-    """If obj is an instave"""
+    '''If obj is an instave'''
     return type(obj) == a_class
