618237273e26438ffdc5d93678242ab48073faae
Udoozy <agbo4samson@gmail.com>
2024-05-01 00:22:24 -0700

    9-rectangle.py

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module for the 9 task"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """An inherited Class"""
+
+    def __init__(self, width, height):
+        """initialisation"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Implemeting the Area"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """This return the format described"""
+        output = '[Rectangle] {}/{}'.format(self.__width/self.__height)
+        return output
