8b1262860a70a302652c53b7017c47f4b07d996b
Udoozy <agbo4samson@gmail.com>
2024-05-04 07:30:15 -0700

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for the task 0"""
+
+
+def read_file(filename=""):
+    """Defined file name function"""
+    with open('filename', 'r') as my_file:
+        print(read.my_file(), end="")
