2b0c50ba7691ee7b627cd265b2a07c6c759011f4
Udoozy <agbo4samson@gmail.com>
2024-05-23 13:56:19 -0700

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,8 +5,10 @@ CREATE TABLE IF NOT EXISTS second_table (
 	score INT
 );
 
+-- Insert the specified records
 INSERT INTO 
 	second_table (id, name, score)
+
 VALUES 
 	(1, "John", 10),
 	(2, "Alex", 3),
