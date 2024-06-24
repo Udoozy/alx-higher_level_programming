1cdf1383d000e7c846db1913ca635db7621a7b5f
Udoozy <agbo4samson@gmail.com>
2024-06-24 12:05:06 -0700

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """Module for task 7"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
+import sys
 
 if __name__ == "__main__":
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-    from sqlalchemy.ext.declarative import declarative_base
-    from model_state import Base, State
-    import sys
-
     args = sys.argv
-    if len(args) < 4:
+    if len(args) != 4:
         exit(1)
     str_con = 'mysql-mysqldb://{}:{}@localhost:3306/{}'
     engine = create_engine(str_con.format(args[1], args[2], args[3]))
