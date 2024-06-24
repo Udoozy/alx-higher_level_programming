2032ea2b12e07c6f7f3741a99856e6061500d740
Udoozy <agbo4samson@gmail.com>
2024-06-24 06:23:35 -0700

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,9 +12,8 @@ if __name__ == "__main__":
             port=3306
             )
     cursor = db.cursor()
-    search_pattern = sys.argv[4]
-    cursor.execute("SELECT * FROM states WHERE name
-                   LIKE %s", (search_pattern, ))
+    match = sys.argv[4]
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
 
     rows = cursor.fetchall()
     for row in rows:
