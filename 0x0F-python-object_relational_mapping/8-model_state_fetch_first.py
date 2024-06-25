04529b7f4602106ede6a08ce01a26525d930326e
Udoozy <agbo4samson@gmail.com>
2024-06-25 07:12:41 -0700

    8-model_state_fetch_first.py

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module is for task 8"""
+import sys
+from mysqlalchemy import sessionmaker
+from mysqlalchemy import (create_engine)
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = ceate_engine('mysql_engine://{}:{}@localhost:3306/{}'
+                          .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state_table = session.query(State).first()
+    if state_data is None:
+        print("Nothing")
+    else:
+        print(state_data.id, state_data.name, sep": ")
