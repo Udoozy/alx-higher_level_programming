a33ba6f2f5018cc96aa211162ebed93567db4dd4
Udoozy <agbo4samson@gmail.com>
2024-07-05 19:11:39 -0700

    4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Python script that fetches https://intranet.hbtn.io/status"""
+import requests
+
+
+if __name__ == '__main__':
+    status = requests.Session()
+
+    response = status.get('https://intranet.hbtn.io/status')
+    body = response.text
+
+    print('Body response:')
+    print('\t- type: {}'.format(type(body)))
+    print('\t- content: {}'.format(body))
