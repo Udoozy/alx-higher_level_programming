d89b7a3baa9b6215d55e8847dc6439c7269d754a
Udoozy <agbo4samson@gmail.com>
2024-07-30 21:45:59 +0100

    1-writeme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (error, argContent) {
-	console.log(error || argContent);
+  console.log(error || argContent);
 });
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const fileSystem = require('fs');
 fileSystem.writeFile(process.argv[2], process.argv[3], error => {
-	if (error) console.log(error);
+  if (error) console.log(error);
 });
