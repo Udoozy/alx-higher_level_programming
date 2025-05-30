3b2368963035a81eabdf6fb678da36fe0a24b552
Udoozy <agbo4samson@gmail.com>
2025-05-30 15:27:48 +0100

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 if __name__ == '__main__':
     import sys
@@ -8,10 +8,10 @@ if __name__ == '__main__':
 
     if argc == 0:
         print("0 arguments.")
-    elif argc == 1:
+    if argc == 1:
         print("1 argument:")
-    else:
-        print(f"{argc} arguments:")
+    
+    print(f"{argc} arguments:")
 
     i = 1
     for arg in argv:
