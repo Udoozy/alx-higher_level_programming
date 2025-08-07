d35042f42fbbae36d99f5750ae346fe59a738d74
Udoozy <agbo4samson@gmail.com>
2025-08-07 03:40:36 +0100

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import doctest
 """A module for adding two integers.
 It takes two arguments.
 Those argument can be float or integer.
@@ -10,8 +11,13 @@ def add_integer(a, b=98):
     """User defined function that add two integers
     a,b(int): The first Argument
     """
-    if type(a) is not int and type(a) is not float:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
