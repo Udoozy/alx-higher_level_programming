0c63df2072629c5ef67109410f0cf0b4d19e6ea6
Udoozy <agbo4samson@gmail.com>
2025-08-26 17:45:59 +0100

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""Module that locks users from
+   creating instances other than first_name
+"""
+
+
 class LockedClass:
+    """Class that perform that uses __slots__
+       to lock object instances
+    """
     __slots__ = ["first_name"]
