01eec10cf0d5ccffec0469b5f0a9efec91dfa49f
Udoozy <agbo4samson@gmail.com>
2025-09-19 17:03:51 +0100

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module is for class and covert to json"""
+
+
+class Student:
+    """Class that converts to dict"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
