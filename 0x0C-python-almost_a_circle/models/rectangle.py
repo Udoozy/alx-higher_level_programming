f6f7a1a014b60f65053165687b9afe082276f951
Udoozy <agbo4samson@gmail.com>
2025-09-21 10:21:31 +0100

    rectangle.py

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Rectagle Class module"""
+
+
+from models.base import Base
+
+class Rectangle(Base):
+    """The Rectangle class That inherit from Base class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
