6925b46d04e4ea2ff9cc48d03ec05b2c4d8b049c
Udoozy <agbo4samson@gmail.com>
2025-09-21 10:07:04 +0100

    models

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,9 +4,13 @@
 
 class Base:
     """The Base class"""
+
+
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-        self.id = id
+        else:
+            self.id = id
