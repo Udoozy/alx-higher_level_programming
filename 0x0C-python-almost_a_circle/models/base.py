ff3dcb6afbfad84b38e753947a65ef2ec47aebce
Udoozy <agbo4samson@gmail.com>
2025-10-01 05:55:51 +0100

    models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,7 +5,6 @@ import json
 
 class Base:
     """The Base class"""
-
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -19,7 +18,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Static method that conver to JSON"""
-        if list_dictionaries == None or list_dictionaries == []:
+        if list_dictionaries is None or list_dictionaries is []:
             return "[]"
         else:
             json_format = json.dumps(list_dictionaries)
